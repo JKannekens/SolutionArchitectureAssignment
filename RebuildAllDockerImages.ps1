97d4659832ad9109ffa33d56ba349841b97e198b
Max Koning <mkoning7@avans.nl>
2019-05-17 12:45:22 +0200

    mongodb added to docker compose, rebuild and stop script added

---
+++ RebuildAllDockerImages.ps1
@@ -0,0 +1,10 @@
+echo "====================="
+echo "====== Volumes ======"
+echo "====================="
+docker volume create --name=sqlserverdata
+docker volume create --name=rabbitmqdata
+docker volume create --name=mongodata
+
+# Rebuild all the services that have changes
+# If you want to (re)build only a specific service, go to the src folder and execute `docker-compose build <servicename-lowercase>`
+docker-compose build --force-rm
