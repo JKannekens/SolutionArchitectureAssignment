a569d44d276efad26ef1586034c203fffe503faa
Max Koning <maxkoning@casema.nl>
2019-05-18 22:52:49 +0200

    update topics

--- Node.Infastructure.Messaging/listen_to_message.js
+++ Node.Infastructure.Messaging/listen_to_message.js
@@ -1,4 +1,5 @@
 var receive = require("./Receive.js");
 
-receive.receive("doctor", "*.*");
-receive.receive("patient", "*.*");
+receive.receive("doctor", "doctor.create");
+receive.receive("doctor", "doctor.update");
+receive.receive("patient", "patient.create");
