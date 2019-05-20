ab08cf34753b0d1dad515c02717e87c370e236cc
Thomas Mout <stmout@student.avans.nl>
2019-05-20 21:19:46 +0200

    Added time service

---
+++ TimeService/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Send {
+	class Program {
+		private static DateTime lastDayCheck;
+		private static DateTime lastTimeCheck;
+		private static DateTime lastMinuteCheck;
+
+		static void Main (string[] args) {
+			IConnection connection = CreateConnection ();
+			using (var channel = connection.CreateModel ()) {
+				channel.QueueDeclare (queue: "time",
+					durable : false,
+					exclusive : false,
+					autoDelete : false,
+					arguments : null);
+
+				Console.WriteLine ("----- Time service started ------");
+				while (true) {
+					CheckTime (channel);
+					CheckDay (channel);
+					CheckMinute (channel);
+					Thread.Sleep (6000);
+				}
+			}
+		}
+
+		private static IConnection CreateConnection () {
+			var factory = new ConnectionFactory () { HostName = "localhost" };
+			return factory.CreateConnection ();
+		}
+
+		private static void CheckMinute (IModel channel) {
+			if (DateTime.Now.Subtract (lastMinuteCheck).Minutes > 0) {
+				Console.WriteLine ("A minute has passed");
+				lastMinuteCheck = DateTime.Now;
+
+				var json = "{ \"Hour\": \"" + lastMinuteCheck.Hour + "\", \"Minute\": \"" + lastMinuteCheck.Minute + "\"  }";
+				PublishEvent (channel, "minute", json);
+			}
+		}
+
+		private static void CheckTime (IModel channel) {
+			if (DateTime.Now.Subtract (lastDayCheck).Hours > 0) {
+				Console.WriteLine ("An hour has passed");
+				lastDayCheck = DateTime.Now;
+
+				var json = "{ \"Hour\": \"" + lastDayCheck.Hour + "\", \"Minute\": \"" + lastDayCheck.Minute + "\"  }";
+				PublishEvent (channel, "hour", json);
+			}
+		}
+
+		private static void CheckDay (IModel channel) {
+			if (DateTime.Now.Subtract (lastTimeCheck).Days > 0) {
+				Console.WriteLine ("A day has passed");
+				lastTimeCheck = DateTime.Now;
+
+				var json = "{ \"Year\": + \"" + lastTimeCheck.Year + "\", \"Month\": \"" + lastTimeCheck.Month + "\", \"DayOfTheWeek\": \"" + lastTimeCheck.DayOfWeek + "\" }";
+				PublishEvent (channel, "day", json);
+			}
+		}
+
+		private static void PublishEvent (IModel channel, string routingKey, string message) {
+			var body = Encoding.UTF8.GetBytes (message);
+
+			var properties = channel.CreateBasicProperties ();
+			properties.Persistent = true;
+
+			channel.BasicPublish (exchange: "",
+				routingKey : routingKey,
+				basicProperties : properties,
+				body : body);
+		}
+	}
+}
