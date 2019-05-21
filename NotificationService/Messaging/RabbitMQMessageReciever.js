778186cb8595ceb44600d31b7ad01c509da1baa1
Thomas Mout <stmout@student.avans.nl>
2019-05-21 13:58:58 +0200

    Added notification service

---
+++ NotificationService/Messaging/RabbitMQMessageReciever.js
@@ -0,0 +1,44 @@
+var amqp = require('amqplib/callback_api');
+const Event = require('../Model/event.model.ts');
+
+module.exports = {
+    receive: function (exchange, arg) {
+        amqp.connect('amqp://rabbitmquser:DEBmbwkSrzy9D1T9cJfa@rabbitmq', function (error0, connection) {
+            if (error0) {
+                return setTimeout(module.exports.receive, 10000, exchange, arg);
+            }
+            connection.createChannel(function (error1, channel) {
+                if (error1) {
+                    throw error1;
+                }
+
+                channel.assertExchange(exchange, 'topic', {
+                    durable: false
+                });
+
+                channel.assertQueue('', {
+                    exclusive: true
+                }, function (error2, q) {
+                    if (error2) {
+                        throw error2;
+                    }
+                    console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+                    channel.bindQueue(q.queue, exchange, arg);
+
+                    channel.consume(q.queue, function (msg) {
+                        module.exports.handleMessage(msg.fields.routingKey, JSON.parse(msg.content));
+                    }, {
+                            noAck: true
+                        });
+                });
+            });
+        });
+    },
+
+    handleMessage: function (routingkey, message) {
+        if (routingkey == "minute") {
+            console.log(message);
+        }
+    }
+};
