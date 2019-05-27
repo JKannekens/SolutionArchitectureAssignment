5de61d39aca03313effb16985be3026380397a1e
Unknown <thomas_641991@hotmail.com>
2019-05-27 12:39:49 +0200

    All scripts merged together for quick restart

---
+++ CompleteRestartContainer.ps1
@@ -0,0 +1,19 @@
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+echo "====================="
+echo "====== Volumes ======"
+echo "====================="
+docker volume create --name=mysqldata
+docker volume create --name=rabbitmqdata
+docker volume create --name=mongodata
+
+# Rebuild all the services that have changes
+# If you want to (re)build only a specific service, go to the src folder and execute `docker-compose build <servicename-lowercase>`
+docker-compose build --force-rm
+
+# Run all containers in the background, and directly afterwards tail the logs files (like"
+# running in the foreground. When pressing crtl-c, youre containers will continue to run."
+# Updated something? Then run the RebuildAllDockerImages script to run changes."
+
+docker-compose up -d; docker-compose logs -f
